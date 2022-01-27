0437d6fe7facb6e39e8c160b57d43369a73a4b11
Stuart Farnaby <stuart@bigbearstudios.co.uk>
2022-01-27 21:15:30 +0000

    Changed *LWEventDelegate to GameLWEventDelegate

--- Runtime/GameLWEventSystem.cs
+++ Runtime/GameLWEventSystem.cs
@@ -8,33 +8,42 @@ namespace BBUnity {
     /// via the Event wrapper and just forwards the caller / data along. 
     /// </summary>
     public class GameLWEventSystem {
-        public delegate void LWEventDelegate(string eventName, object caller, object data);
+        public delegate void GameLWEventDelegate(string eventName, object caller, object data);
 
-        private Dictionary<int, Dictionary<int, LWEventDelegate>> _events;
+        /// <summary>
+        /// Reference to the global (shared) event system. 
+        /// </summary>
+        static private GameLWEventSystem __globalLWEventSystem = null;
+
+        private Dictionary<int, Dictionary<int, GameLWEventDelegate>> _events;
 
         public GameLWEventSystem() {
-            _events = new Dictionary<int, Dictionary<int, LWEventDelegate>>();
+            _events = new Dictionary<int, Dictionary<int, GameLWEventDelegate>>();
+        }
+
+        static GameLWEventSystem() {
+            __globalLWEventSystem = new GameLWEventSystem();
         }
 
         /*
          * Registeration of Event Listeners
          */
 
-        public void ListenFor(string eventName, LWEventDelegate onEvent) {
+        public void ListenFor(string eventName, GameLWEventDelegate onEvent) {
             AddInternalListener(Internal.Events.Utilities.HashCodeForEventName(eventName), onEvent);
         }
 
-        public void Add(string eventName, LWEventDelegate onEvent) {
+        public void Add(string eventName, GameLWEventDelegate onEvent) {
             AddInternalListener(Internal.Events.Utilities.HashCodeForEventName(eventName), onEvent);
         }
 
-        private void AddInternalListener(int eventHashCode, LWEventDelegate onEvent) {
-            int objectHashCode = Internal.Events.Utilities.HashCodeForLWEventDelegate(onEvent);
+        private void AddInternalListener(int eventHashCode, GameLWEventDelegate onEvent) {
+            int objectHashCode = Internal.Events.Utilities.HashCodeForGameLWEventDelegate(onEvent);
 
-            if(_events.TryGetValue(eventHashCode, out Dictionary<int, LWEventDelegate> events)) {
+            if(_events.TryGetValue(eventHashCode, out Dictionary<int, GameLWEventDelegate> events)) {
                 events[objectHashCode] = onEvent;
             } else {
-                _events[eventHashCode] = new Dictionary<int, LWEventDelegate>() { { objectHashCode, onEvent } };
+                _events[eventHashCode] = new Dictionary<int, GameLWEventDelegate>() { { objectHashCode, onEvent } };
             }
         }
 
@@ -68,8 +77,8 @@ namespace BBUnity {
 
         private void SendInternalEvent(string eventName, object caller = null, object data = null) {
             int eventHashCode = Internal.Events.Utilities.HashCodeForEventName(eventName);
-            if(_events.TryGetValue(eventHashCode, out Dictionary<int, LWEventDelegate> value)) {
-                foreach(LWEventDelegate del in value.Values) {
+            if(_events.TryGetValue(eventHashCode, out Dictionary<int, GameLWEventDelegate> value)) {
+                foreach(GameLWEventDelegate del in value.Values) {
                     del(eventName, caller, data);
                 }
             }
@@ -81,7 +90,7 @@ namespace BBUnity {
 
         public void Remove(object listener) {
             int objectHashCode = listener.GetHashCode();
-            foreach(Dictionary<int, LWEventDelegate> events in _events.Values) {
+            foreach(Dictionary<int, GameLWEventDelegate> events in _events.Values) {
                 if(events.ContainsKey(objectHashCode)) {
                     events.Remove(objectHashCode);
                 }
@@ -94,7 +103,7 @@ namespace BBUnity {
 
         private void RemoveListener(int eventHashCode, object listener) {
             int objectHashCode = listener.GetHashCode();
-            if(_events.TryGetValue(eventHashCode, out Dictionary<int, LWEventDelegate> events)) {
+            if(_events.TryGetValue(eventHashCode, out Dictionary<int, GameLWEventDelegate> events)) {
                 if(events.ContainsKey(objectHashCode)) {
                     events.Remove(objectHashCode);
                 }
@@ -115,7 +124,7 @@ namespace BBUnity {
 
         private void RemoveListeners(int eventHashCode, bool preserveLookups) {
             if(preserveLookups) { //Remove the values from within the lookup, but not the lookup itself
-                if(_events.TryGetValue(eventHashCode, out Dictionary<int, LWEventDelegate> events)) {
+                if(_events.TryGetValue(eventHashCode, out Dictionary<int, GameLWEventDelegate> events)) {
                     events.Clear();
                 }   
             } else { //Removes all of the listeners lookups
@@ -135,12 +144,16 @@ namespace BBUnity {
 
         public void Clear(bool preserveLookups) {
             if(preserveLookups) {
-                foreach(Dictionary<int, LWEventDelegate> events in _events.Values) {
+                foreach(Dictionary<int, GameLWEventDelegate> events in _events.Values) {
                     events.Clear();
                 }
             } else {
                 Clear();
             }
+        }
+
+        static public GameLWEventSystem Global {
+            get { return __globalLWEventSystem; }
         }
     }
 }
